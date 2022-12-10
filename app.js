218503328313f0b421348675ef1a8f3590a914f4
Julie Nisbet <jmnisbet@gmail.com>
2022-12-10 15:01:59 -0800

    javascript building blocks

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-// grab DOM elements
+console.log('hello world from app.js');
 
-// set event listeners
+// data types
+// numbers
+42;
+// strings
+const answer = '42';
+// can't do this to constants!
+// answer = 12;
+let dogName = 'Ben Donich';
+console.log(`My dog's name is ${dogName}. ${dogName} is very cute. ${dogName} is 7 years old.`);
+
+// arrays
+let colors = ['red', 'green', 'blue'];
+console.log(colors);
+console.log(colors[2]);
+
+// objects
+let benny = {
+    name: 'Benny',
+    age: 7,
+    breeds: ['Terrier', 'Chihauhau', 'Pekingese'],
+};
+console.log(benny);
