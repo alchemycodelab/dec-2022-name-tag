3130ab97eabac256a40d31e089470a0a140a53de
Julie Nisbet <jmnisbet@gmail.com>
2022-12-10 16:36:11 -0800

    change name on nametag on keyup

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+// grab the different DOM elements
 const booger = document.getElementById('nametag');
 const nameDisplay = document.getElementById('name-display');
 const nameInput = document.getElementById('name-input');
 const colorInput = document.getElementById('color-input');
 const button = document.getElementById('update-button');
+// add event listener for the click
 button.addEventListener('click', () => {
-    console.log('i am clicking the button');
-    console.log(nameDisplay);
-    // nameDisplay.textContent = 'Julie';
+    // update the nameDisplay with the value in the input
     nameDisplay.textContent = nameInput.value;
+    // change the style to red text
     nameDisplay.style.color = 'red';
+    // change the background color
     booger.style.backgroundColor = colorInput.value;
+});
+
+nameInput.addEventListener('keyup', () => {
+    nameDisplay.textContent = nameInput.value;
 });
